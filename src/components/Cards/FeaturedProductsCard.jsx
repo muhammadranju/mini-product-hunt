d4d394306a38e40be27693e1259d08ed3cadabf7
Muhammad Ranju <mdranju23@gmail.com>
2025-01-15 01:22:12 +0600

    feat: enhance product card components with dynamic data and create FeaturedProductsCard

--- src/components/Cards/FeaturedProductsCard.jsx
+++ src/components/Cards/FeaturedProductsCard.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { FaChevronUp } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const Cards = ({ product }) => {
+const FeaturedProductsCard = ({ product }) => {
+  console.log(product);
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
       <Link to={`/product/${product?.slug}`}>
         <img
-          className="w-full h-64 object-cover rounded-md mb-4"
+          className="w-full h-48 object-cover rounded-md mb-4"
           src={product?.productImage}
           alt={`Trending Product `}
         />
@@ -21,8 +22,8 @@ const Cards = ({ product }) => {
           : product?.description}
       </p>
       <div className="flex items-center space-x-2 space-y-1 flex-wrap my-4">
-        {product?.tags.map((tag) => (
-          <div key={tag} className="badge badge-ghost">
+        {product?.tags.slice(0, 2).map((tag) => (
+          <div key={tag} className="badge badge-ghost text-xs">
             {tag}
           </div>
         ))}
@@ -30,11 +31,11 @@ const Cards = ({ product }) => {
       <div className="flex items-center space-x-2">
         <button className="bg-slate-800 text-slate-50 py-2 px-4 rounded-lg flex items-center space-x-2">
           <FaChevronUp className="mr-1" />
-          Upvote {product?.upvotes}
+          Upvote 25
         </button>
       </div>
     </div>
   );
 };
 
-export default Cards;
+export default FeaturedProductsCard;
