c9139fa8c1b6e1b5c7a03c9a96c315cfd91c2295
Muhammad Ranju <mdranju23@gmail.com>
2025-01-15 22:36:31 +0600

    feat: implement upvote functionality in FeaturedProductsCard with user role validation and navigation

--- src/components/Cards/FeaturedProductsCard.jsx
+++ src/components/Cards/FeaturedProductsCard.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import { AuthContext } from "@/context/AuthProvider";
+import useRole from "@/hooks/useRole";
+import React, { useContext } from "react";
 import { FaChevronUp } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const FeaturedProductsCard = ({ product }) => {
-  console.log(product);
+  const { user } = useContext(AuthContext);
+  const [role] = useRole();
+  const navigate = useNavigate();
+  const handleUpvote = async (productId, ownerId) => {
+    if (!user) {
+      navigate("/auth/login");
+      return;
+    }
+    if (role?.user?.email === ownerId) {
+      alert("You cannot upvote your own product.");
+      return;
+    }
+  };
+
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow">
       <Link to={`/product/${product?.slug}`}>
@@ -13,9 +28,11 @@ const FeaturedProductsCard = ({ product }) => {
           alt={`Trending Product `}
         />
       </Link>
-      <h4 className="text-xl font-semibold mb-2 capitalize">
-        {product?.productName}
-      </h4>
+      <Link to={`/product/${product?.slug}`}>
+        <h4 className="text-xl font-semibold mb-2 cursor-pointer capitalize">
+          {product?.productName}
+        </h4>
+      </Link>
       <p className="text-slate-600 mb-4">
         {product?.description.length > 80
           ? product?.description.slice(0, 50) + "..."
@@ -29,7 +46,15 @@ const FeaturedProductsCard = ({ product }) => {
         ))}
       </div>
       <div className="flex items-center space-x-2">
-        <button className="bg-slate-800 text-slate-50 py-2 px-4 rounded-lg flex items-center space-x-2">
+        <button
+          disabled={role?.user?.email === product?.owner?.ownerEmail}
+          onClick={() => handleUpvote(product?._id, product?.owner?.ownerEmail)}
+          className={`bg-slate-800 text-slate-50 py-2 px-4 rounded-lg flex items-center space-x-2  ${
+            user && role?.user?.email === product?.owner?.ownerEmail
+              ? "opacity-50 cursor-not-allowed"
+              : "hover:bg-blue-600"
+          }`}
+        >
           <FaChevronUp className="mr-1" />
           Upvote 25
         </button>
